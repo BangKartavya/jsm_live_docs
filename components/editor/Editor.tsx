381924da4e93cfb3c005bb4a9031fd53d2785143
BangKartavya <bangkartavya@gmail.com>
2024-11-12 20:38:32 +0530

    fixed editor bug

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -46,7 +46,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
         </div>
         <div className="editor-wrapper flex flex-col items-center justify-start">
           {status === 'not-loaded' || status === 'loading' ? <Loader /> : (
-            <div className="editor-inner min-h-[1100px] relative mb-5 h-fit w-full max-w-[800px] shadow-md lg:mb-10">
+            <div className="editor-inner relative mb-5 h-fit w-full max-w-[800px] shadow-md lg:mb-10">
               <RichTextPlugin
                 contentEditable={
                   <ContentEditable className="editor-input h-full" />
